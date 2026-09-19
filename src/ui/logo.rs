a3953e8fcdd9fc89ba86315718782a9cad6538d5
Lưu Minh Quang <luuminhq29@gmail.com>
2026-09-19 23:59:10 +0700

    feat: improve distro logos and fix release build

--- src/ui/logo.rs
+++ src/ui/logo.rs
@@ -1,5 +1,8 @@
 use crate::system::os::OsInfo;
 
+/// Returns a distro logo suitable for a Fastfetch-style left column.
+/// The artwork is intentionally kept as plain Unicode/ASCII so it works
+/// without runtime downloads, external assets, or special fonts.
 pub fn render(mode: &str, os: &OsInfo) -> Vec<String> {
     match mode.to_lowercase().as_str() {
         "none" => Vec::new(),
@@ -7,7 +10,20 @@ pub fn render(mode: &str, os: &OsInfo) -> Vec<String> {
         "debian" => debian(),
         "arch" => arch(),
         "fedora" => fedora(),
+        "opensuse" => opensuse(),
+        "alpine" => alpine(),
+        "manjaro" => manjaro(),
+        "linuxmint" | "mint" => mint(),
+        "kali" => kali(),
+        "pop" | "popos" => popos(),
+        "gentoo" => gentoo(),
+        "void" => void_linux(),
+        "rocky" => rocky(),
+        "almalinux" | "alma" => alma(),
+        "centos" => centos(),
+        "nixos" => nixos(),
         "linux" => linux(),
+        "custom" => custom(),
         "auto" => render_auto(os),
         _ => render_auto(os),
     }
@@ -19,60 +35,320 @@ fn render_auto(os: &OsInfo) -> Vec<String> {
         "debian" => debian(),
         "arch" | "archlinux" => arch(),
         "fedora" => fedora(),
+        "opensuse-tumbleweed" | "opensuse-leap" | "opensuse" => opensuse(),
+        "alpine" => alpine(),
+        "manjaro" => manjaro(),
+        "linuxmint" => mint(),
+        "kali" => kali(),
+        "pop" => popos(),
+        "gentoo" => gentoo(),
+        "void" => void_linux(),
+        "rocky" => rocky(),
+        "almalinux" => alma(),
+        "centos" => centos(),
+        "nixos" => nixos(),
         _ => linux(),
     }
 }
 
 fn ubuntu() -> Vec<String> {
     vec![
-        "        _        ".into(),
-        "       / \\       ".into(),
-        "      /● ●\\      ".into(),
-        "     /● ● ● ●\\   ".into(),
-        "      \\  /       ".into(),
-        "    UBUNTU       ".into(),
+        "             .-/+oossssoo+/-.",
+        "         `:+ssssssssssssssssss+:`",
+        "       -+ssssssssssssssssssyyssss+-",
+        "     .ossssssssssssssssssdMMMNysssso.",
+        "    /ssssssssssshdmmNNmmyNMMMMhssssss/",
+        "   +ssssssssshmydMMMMMMMNddddyssssssss+",
+        "  /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/",
+        " .ssssssssdMMMNhsssssssssshNMMMdssssssss.",
+        " +sssshhhyNMMNyssssssssssssyNMMMysssssss+",
+        " ossyNMMMNyMMhsssssssssssssshmmmhssssssso",
+        " ossyNMMMNyMMhsssssssssssssshmmmhssssssso",
+        " +sssshhhyNMMNyssssssssssssyNMMMysssssss+",
+        " .ssssssssdMMMNhsssssssssshNMMMdssssssss.",
+        "  /sssssssshNMMMyhhyyyyhmNMMMNhssssssss/",
+        "   +ssssssssshmydMMMMMMMNddddyssssssss+",
+        "    /ssssssssssshdmmNNmmyNMMMMhssssss/",
+        "     .ossssssssssssssssssdMMMNysssso.",
+        "       -+ssssssssssssssssssyyssss+-",
+        "         `:+ssssssssssssssssss+:`",
+        "             .-/+oossssoo+/-.      ",
     ]
+    .into_iter()
+    .map(String::from)
+    .collect()
 }
 
 fn debian() -> Vec<String> {
-    vec![
-        "       .--.      ".into(),
-        "      /.-. '     ".into(),
-        "     /   |       ".into(),
-        "     \\  /        ".into(),
-        "      `--'       ".into(),
-        "     DEBIAN      ".into(),
-    ]
+    lines(&[
+        "       _,met$$$$$gg.",
+        "    ,g$$$$$$$$$$$$$$$P.",
+        "  ,g$$P\"       \"Y$$.",
+        " ,$$P'              `$$$.",
+        "',$$P       ,ggs.     `$$b:",
+        "`d$$'     ,$P\"'   .    $$$",
+        " $$P      d$'     ,    $$P",
+        " $$:      $$.   -    ,d$$'",
+        " $$;      Y$b._   _,d$P'",
+        " Y$$.    `.`\"Y$$$$P\"'",
+        " `$$b      \"-.__",
+        "  `Y$$",
+        "   `Y$$.",
+        "     `$$b.",
+        "       `Y$$b.",
+        "          `\"Y$b._",
+        "              `\"\"",
+    ])
 }
 
 fn arch() -> Vec<String> {
-    vec![
-        "       /\\        ".into(),
-        "      /  \\       ".into(),
-        "     / /\\ \\      ".into(),
-        "    /_/  \\_\\     ".into(),
-        "      ARCH       ".into(),
-    ]
+    lines(&[
+        "                   -`",
+        "                  .o+`",
+        "                 `ooo/",
+        "                `+oooo:",
+        "               `+oooooo:",
+        "               -+oooooo+:",
+        "             `/:-:++oooo+:",
+        "            `/++++/++++++:",
+        "           `/++++++++++++++:",
+        "          `/+++ooooooooooooo/`",
+        "         ./ooosssso++osssssso+`",
+        "        .oossssso-````/ossssss+`",
+        "       -osssssso.      :ssssssso.",
+        "      :osssssss/        osssso++.",
+        "     /ossssssss/        +ssssooo/-",
+        "   `/ossssso+/:-        -:/+osssso+-",
+        "  `+sso+:-`                 `.-/+oso:",
+        " `++:.                           `-/+/",
+        " .`                                 `/",
+    ])
 }
 
 fn fedora() -> Vec<String> {
-    vec![
-        "      ______     ".into(),
-        "     / ____ \\    ".into(),
-        "    | /    \\ |   ".into(),
-        "    | \\____/ |   ".into(),
-        "     \\______/    ".into(),
-        "     FEDORA      ".into(),
-    ]
+    lines(&[
+        "        /:-------------:\\",
+        "       :-------------------::",
+        "      :-----------/shhyo/:--:",
+        "    /-----------omMMMMMMMho-:",
+        "   :-----------yMMMMMMMMMMs:",
+        "  :-----------yMMMMMMMMMMs",
+        " /-----------/NMMMMMMMMMM+",
+        "/-----------/NMMMMMMMMMM+",
+        ":-----------yMMMMMMMMMMs",
+        " :---------/yMMMMMMMMMMs",
+        "  :--------/yMMMMMMMMMMs",
+        "   :-------/yMMMMMMMMMMs",
+        "    /------/yMMMMMMMMMMs",
+        "     :-----/yMMMMMMMMMMs",
+        "      :----/yMMMMMMMMMMs",
+        "       :---/yMMMMMMMMMMs",
+        "        :--/yMMMMMMMMMMs",
+        "         :-/yMMMMMMMMMMs",
+        "          :/yMMMMMMMMMMs",
+        "           `:yMMMMMMMMMMs",
+    ])
+}
+
+fn opensuse() -> Vec<String> {
+    lines(&[
+        "         .;ldkO0000Okdl;.",
+        "       .;d00KKKKKKKKKK00d;.",
+        "      .d0KKKKKKKKKKKKKKKK0d.",
+        "     :0KKKKKKKKKKKKKKKKKKKK0:",
+        "    :0KKKKKKKKKKKKKKKKKKKKKK0:",
+        "   .0KKKKKKKKKKKKKKKKKKKKKKKK0.",
+        "   dKKKKKKKKKKKKKKKKKKKKKKKKKKd",
+        "  lKKKKKKKKKKKKKKKKKKKKKKKKKKKKl",
+        "  0KKKKKKKKKKKKKKKKKKKKKKKKKKKK0",
+        "  0KKKKKKKKKKKKKKKKKKKKKKKKKKKK0",
+        "  lKKKKKKKKKKKKKKKKKKKKKKKKKKKKl",
+        "   dKKKKKKKKKKKKKKKKKKKKKKKKKKd",
+        "    :0KKKKKKKKKKKKKKKKKKKKKK0:",
+        "     :0KKKKKKKKKKKKKKKKKKKK0:",
+        "      .d0KKKKKKKKKKKKKKKK0d.",
+        "       .;d00KKKKKKKKKK00d;.",
+        "         .;ldkO0000Okdl;.",
+    ])
+}
+
+fn alpine() -> Vec<String> {
+    lines(&[
+        "       /\\      _      ",
+        "      /  \\___ / \\     ",
+        "     / /\\   / /\\ \\    ",
+        "    / /  \\_/ /  \\ \\   ",
+        "   /_/    /__/    \\_\\  ",
+        "      A L P I N E       ",
+    ])
+}
+
+fn manjaro() -> Vec<String> {
+    lines(&[
+        "████████████████████████████",
+        "████████████████████████████",
+        "███████  ███████  █████████",
+        "███████  ███████  █████████",
+        "███████  ███████  █████████",
+        "███████  ███████  █████████",
+        "███████  ███████  █████████",
+        "████████████████████████████",
+        "████████████████████████████",
+    ])
+}
+
+fn mint() -> Vec<String> {
+    lines(&[
+        "             .88888888:.",
+        "          888888888888888888.",
+        "        .8888888888888888888888.",
+        "       .888888888888888888888888.",
+        "       88888888888888888888888888",
+        "       88888888888888888888888888",
+        "       88888888888888888888888888",
+        "       88888888888888888888888888",
+        "        `8888888888888888888888`",
+        "          `888888888888888888`",
+        "             `:88888888:`",
+    ])
+}
+
+fn kali() -> Vec<String> {
+    lines(&[
+        "            ...............",
+        "        .:::::::::::::::::::::. ",
+        "      .:::::::::::::::::::::::::.",
+        "     :::::::::::::::::::::::::::::",
+        "    ::::::::::::-::::::::::::::::",
+        "   ::::::::::::-:::::::::::::::::",
+        "   :::::::::::::::::::::::::::::::",
+        "    :::::::::::::::::::::::::::::",
+        "     :::::::::::::::::::::::::::",
+        "       .:::::::::::::::::::::.  ",
+        "           `:::::::::::::`     ",
+        "               `::::`          ",
+    ])
+}
+
+fn popos() -> Vec<String> {
+    lines(&[
+        "          .o+`                 ",
+        "         `ooo/                 ",
+        "        `+oooo:                ",
+        "       `+oooooo:               ",
+        "       -+oooooo+:              ",
+        "     `/:-:++oooo+:             ",
+        "    `/++++/+++++++:            ",
+        "   `/++++++++++++++:           ",
+        "  `/+++ooooooooooooo/`         ",
+        " ./ooosssso++osssssso+`        ",
+        ".oossssso-````/ossssss+`      ",
+        "-osssssso.      :ssssssso.    ",
+    ])
+}
+
+fn gentoo() -> Vec<String> {
+    lines(&[
+        "        _-----_        ",
+        "       (       \\      ",
+        "      (         )      ",
+        "      (  GENTOO )      ",
+        "       (       /       ",
+        "        `-----'        ",
+        "      __/   \\__       ",
+        "     /           \\     ",
+    ])
+}
+
+fn void_linux() -> Vec<String> {
+    lines(&[
+        "        ________        ",
+        "       /  _____/        ",
+        "      /   \\  ___       ",
+        "     /    /   \\  \\     ",
+        "    /____/     \\__\\    ",
+        "       V O I D          ",
+    ])
+}
+
+fn rocky() -> Vec<String> {
+    lines(&[
+        "          /\\            ",
+        "         /  \\           ",
+        "        / /\\ \\          ",
+        "       / /  \\ \\         ",
+        "      /_/____\\_\\        ",
+        "      ROCKY LINUX         ",
+    ])
+}
+
+fn alma() -> Vec<String> {
+    lines(&[
+        "          .--------.      ",
+        "        .'          '.    ",
+        "       /   .------.   \\   ",
+        "      ;   /        \\   ;  ",
+        "      |  |  ALMA    |  |  ",
+        "      ;   \\        /   ;  ",
+        "       \\   '------'   /   ",
+        "        '.          .'    ",
+        "          '--------'      ",
+    ])
+}
+
+fn centos() -> Vec<String> {
+    lines(&[
+        "       .---------.        ",
+        "      /  .-----.  \\       ",
+        "     /  /       \\  \\      ",
+        "    |  |  CENTOS |  |     ",
+        "     \\  \\       /  /      ",
+        "      \\  '-----'  /       ",
+        "       '---------'        ",
+    ])
+}
+
+fn nixos() -> Vec<String> {
+    lines(&[
+        "        \\   /\\   /       ",
+        "         \\ /  \\ /        ",
+        "      ---- NIXOS ----      ",
+        "         / \\  / \\        ",
+        "        /   \\/   \\       ",
+        "       /___________\\      ",
+    ])
 }
 
 fn linux() -> Vec<String> {
-    vec![
-        "       ██████       ".into(),
-        "     ██████████     ".into(),
-        "    ███  ███  ███   ".into(),
-        "    ███  ███  ███   ".into(),
-        "    █████████████    ".into(),
-        "       LINUX        ".into(),
-    ]
+    lines(&[
+        "           .-::::::::::-.     ",
+        "        .:+ssssssssssssss+:.  ",
+        "      -+ssssssssssssssssssss+-",
+        "    .osssssssssssssssssssssssso.",
+        "   /sssssssssssssssssssssssssss/",
+        "  +ssssssssssssssssssssssssssss+",
+        " /ssssssssssssssssssssssssssssss/",
+        " ossssssssssssssssssssssssssssso",
+        " +ssssssssssssssssssssssssssss+",
+        "  /ssssssssssssssssssssssssss/",
+        "   .osssssssssssssssssssssso.",
+        "     -+ssssssssssssssssss+-",
+        "        .:+ssssssssss+:.",
+        "           .-::::::-.     ",
+    ])
+}
+
+fn custom() -> Vec<String> {
+    let path = std::env::var_os("HOME")
+        .map(std::path::PathBuf::from)
+        .map(|h| h.join(".config/astrafetch/logo.txt"));
+
+    path.and_then(|p| std::fs::read_to_string(p).ok())
+        .map(|s| s.lines().map(String::from).collect())
+        .filter(|lines: &Vec<String>| !lines.is_empty())
+        .unwrap_or_else(linux)
+}
+
+fn lines(lines: &[&str]) -> Vec<String> {
+    lines.iter().map(|line| (*line).to_string()).collect()
 }
--- src/ui/static_view.rs
+++ src/ui/static_view.rs
@@ -9,88 +9,85 @@ pub fn run(c: Config) -> Result<()> {
     let s = SystemSnapshot::collect(&c)?;
 
     if crate::ui::colors::no_color() {
-        println!("{}", plain(&s, &c));
+        println!("{}", plain(&s));
         return Ok(());
     }
 
     let ls = logo::render(&c.logo, &s.os);
-
-    let info = vec![
-        format!("OS          {}", s.os.version),
-        format!("Kernel      {}", s.kernel),
-        format!("Host        {}", s.hostname),
-        format!("Arch        {}", s.architecture),
-        format!(
-            "Uptime      {}",
-            format_uptime(std::time::Duration::from_secs(s.uptime_seconds))
-        ),
-        format!("CPU         {} ({:.0}%)", s.cpu.model, s.cpu.usage_percent),
-        format!(
-            "Memory      {} / {}",
-            crate::system::memory::format_bytes(s.memory.used_bytes),
-            crate::system::memory::format_bytes(s.memory.total_bytes)
-        ),
-        format!(
-            "Disk        {} / {} ({:.0}%)",
-            crate::system::memory::format_bytes(s.disk.used_bytes),
-            crate::system::memory::format_bytes(s.disk.total_bytes),
-            s.disk.usage_percent
-        ),
-        format!("GPU         {}", s.gpu.model),
-        format!("Shell       {}", s.shell.name),
-        format!("Terminal    {}", s.terminal.name),
-        format!("DE          {}", s.desktop.desktop),
-        format!("Display     {}", s.desktop.display),
-    ];
-
-    let width = info.iter().map(String::len).max().unwrap_or(30) + 4;
-
-    println!("\x1b[1;36m╔{}╗\x1b[0m", "═".repeat(width));
-
-    let title = "ASTRΛFETCH";
-    println!("║ \x1b[1;36m{title:^width$}\x1b[0m ║");
+    let info = info_lines(&s);
+    let logo_width = ls.iter().map(display_width).max().unwrap_or(0).max(28) + 2;
 
     let n = ls.len().max(info.len());
-
     for i in 0..n {
-        let left = ls.get(i).cloned().unwrap_or_default();
-        let right = info.get(i).cloned().unwrap_or_default();
-
-        println!("║ \x1b[36m{left:<24}\x1b[0m ║ {right:<width$}║");
+        let left = ls.get(i).map(String::as_str).unwrap_or("");
+        let right = info.get(i).map(String::as_str).unwrap_or("");
+        let padded = format!("{left:<width$}", width = logo_width);
+        println!("\x1b[1;36m{padded}\x1b[0m  {right}");
     }
 
-    println!("╚{}╝", "═".repeat(width + 28));
-
     if c.author {
-        println!("\x1b[90mAstraFetch\nby Lưu Minh Quang - Astra\x1b[0m");
+        println!();
+        println!("\x1b[90mAstraFetch  •  by Lưu Minh Quang - Astra\x1b[0m");
     }
 
     Ok(())
 }
 
-fn plain(s: &SystemSnapshot, _: &Config) -> String {
+fn info_lines(s: &SystemSnapshot) -> Vec<String> {
+    vec![
+        format!("\x1b[1;36m{}@{}\x1b[0m", username(), s.hostname),
+        "\x1b[90m----------------\x1b[0m".to_string(),
+        format!("\x1b[1;36mOS:\x1b[0m {}", s.os.version),
+        format!("\x1b[1;36mHost:\x1b[0m {}", s.hostname),
+        format!("\x1b[1;36mKernel:\x1b[0m {}", s.kernel),
+        format!(
+            "\x1b[1;36mUptime:\x1b[0m {}",
+            format_uptime(std::time::Duration::from_secs(s.uptime_seconds))
+        ),
+        format!("\x1b[1;36mCPU:\x1b[0m {}", s.cpu.model),
+        format!("\x1b[1;36mGPU:\x1b[0m {}", s.gpu.model),
+        format!(
+            "\x1b[1;36mMemory:\x1b[0m {} / {} ({:.0}%)",
+            crate::system::memory::format_bytes(s.memory.used_bytes),
+            crate::system::memory::format_bytes(s.memory.total_bytes),
+            s.memory.usage_percent
+        ),
+        format!(
+            "\x1b[1;36mDisk (/):\x1b[0m {} / {} ({:.0}%)",
+            crate::system::memory::format_bytes(s.disk.used_bytes),
+            crate::system::memory::format_bytes(s.disk.total_bytes),
+            s.disk.usage_percent
+        ),
+        format!("\x1b[1;36mShell:\x1b[0m {}", s.shell.name),
+        format!("\x1b[1;36mTerminal:\x1b[0m {}", s.terminal.name),
+        format!("\x1b[1;36mDE:\x1b[0m {}", s.desktop.desktop),
+        format!("\x1b[1;36mDisplay:\x1b[0m {}", s.desktop.display),
+    ]
+}
+
+fn display_width(s: &str) -> usize {
+    s.chars().count()
+}
+
+fn username() -> String {
+    std::env::var("USER").unwrap_or_else(|_| "user".to_string())
+}
+
+fn plain(s: &SystemSnapshot) -> String {
     format!(
-        "AstraFetch\n\
-         OS: {}\n\
-         Kernel: {}\n\
-         Host: {}\n\
-         Arch: {}\n\
-         Uptime: {}\n\
-         CPU: {}\n\
-         Memory: {} / {}\n\
-         GPU: {}\n\
-         Shell: {}\n\
-         Terminal: {}\n\
-         DE: {}\n",
+        "AstraFetch\nOS: {}\nHost: {}\nKernel: {}\nUptime: {}\nCPU: {}\nGPU: {}\nMemory: {} / {} ({:.0}%)\nDisk: {} / {} ({:.0}%)\nShell: {}\nTerminal: {}\nDE: {}\n",
         s.os.version,
-        s.kernel,
         s.hostname,
-        s.architecture,
+        s.kernel,
         format_uptime(std::time::Duration::from_secs(s.uptime_seconds)),
         s.cpu.model,
+        s.gpu.model,
         crate::system::memory::format_bytes(s.memory.used_bytes),
         crate::system::memory::format_bytes(s.memory.total_bytes),
-        s.gpu.model,
+        s.memory.usage_percent,
+        crate::system::memory::format_bytes(s.disk.used_bytes),
+        crate::system::memory::format_bytes(s.disk.total_bytes),
+        s.disk.usage_percent,
         s.shell.name,
         s.terminal.name,
         s.desktop.desktop,
